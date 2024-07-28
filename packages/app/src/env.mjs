04343496dd937597ea14e09e55773230e218d3d1
capiloky <jauregui.alvaro@nimioestudio.com>
2024-07-28 11:29:43 +0200

    chore: Update env.mjs to include NEXTAUTH_URL and NEXTAUTH_SECRET

--- packages/app/src/env.mjs
+++ packages/app/src/env.mjs
@@ -3,13 +3,17 @@ import { z } from "zod";
 
 export const env = createEnv({
     server: {
-        DATABASE_URL: z.string().min(1)
+        DATABASE_URL: z.string().min(1),
+        NEXTAUTH_URL: z.string().min(1),
+        NEXTAUTH_SECRET: z.string().min(1)
     },
     client: {
         NEXT_PUBLIC_APP_URL: z.string().min(1)
     },
     runtimeEnv: {
         NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-        DATABASE_URL: process.env.DATABASE_URL
+        DATABASE_URL: process.env.DATABASE_URL,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
     }
 });
