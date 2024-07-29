a2282affb59e3efe70fc452411fe07a98cd2cf78
capiloky <jauregui.alvaro@nimioestudio.com>
2024-07-30 01:02:57 +0200

    refactor: update checkSecurity function to include resource security check

--- packages/api/src/utils/security.ts
+++ packages/api/src/utils/security.ts
@@ -1,21 +1,34 @@
 import {
     Context,
-    ModelResult,
+    ModelResource,
     Operation,
     PrismaModelName,
     User
 } from "../types";
 
-async function checkSecurity<TModel extends PrismaModelName>({
-    operation,
-    context
-}: {
+type CheckSecurityProps<TModel extends PrismaModelName> = {
+    resource: ModelResource<TModel>;
     operation: Operation<TModel>;
     context: Context<TModel>;
-}): Promise<boolean> {
+};
+
+async function checkSecurity<TModel extends PrismaModelName>({
+    resource,
+    operation,
+    context
+}: CheckSecurityProps<TModel>): Promise<boolean> {
+    // First, check if the operation has a security function
     if (operation.security) {
         return operation.security({
-            object: context.previousObject as ModelResult<TModel>,
+            object: context.previousObject,
+            user: context.security.user as User
+        });
+    }
+
+    // Next, check if the resource has a security function
+    if (resource.security) {
+        return resource.security({
+            object: context.previousObject,
             user: context.security.user as User
         });
     }
