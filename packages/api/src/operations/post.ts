e19ce241f5af5973d34f11ff1797e7f293eb62fc
capiloky <jauregui.alvaro@nimioestudio.com>
2024-07-30 01:00:25 +0200

    refactor: update postHandler function to include pre and post persist operations

--- packages/api/src/operations/post.ts
+++ packages/api/src/operations/post.ts
@@ -1,41 +1,72 @@
 import {
     Context,
+    MutateModelResult,
     PostOperation,
     PrismaModelName,
-    UriVariables
+    UriVariables,
+    Validation
 } from "../types";
+
+type PostHandlerProps<
+    TModel extends PrismaModelName,
+    TContext extends Context<TModel>,
+    TInputValidation extends Validation<TModel>
+> = {
+    request: Request;
+    model: PrismaModelName;
+    operation: PostOperation<TModel, TInputValidation>;
+    uriVariables: UriVariables;
+    context: TContext;
+};
 
 async function postHandler<
     TModel extends PrismaModelName,
-    TContext extends Context<TModel>
+    TContext extends Context<TModel>,
+    TInputValidation extends Validation<TModel>
 >({
     request,
     model,
     operation,
     uriVariables,
     context
-}: {
-    request: Request;
-    model: PrismaModelName;
-    operation: PostOperation<TModel>;
-    uriVariables: UriVariables;
-    context: TContext;
-}): Promise<Response> {
-    const data = operation.inputValidation?.parse(request.body) || request.body;
+}: PostHandlerProps<TModel, TContext, TInputValidation>): Promise<Response> {
+    const data =
+        operation.inputValidation?.parse(request.body) || (request.body as any);
 
     const { select } = operation;
 
-    const result = await (context.db[model] as any).create({
+    if (operation.onPrePersist) {
+        await operation.onPrePersist({
+            data,
+            operation,
+            uriVariables,
+            context
+        });
+    }
+
+    const result = (await (context.db[model] as any).create({
         data,
         select
-    });
+    })) as MutateModelResult<TModel>;
+
+    if (operation.onPostPersist) {
+        await operation.onPostPersist({
+            data: result,
+            operation,
+            uriVariables,
+            context
+        });
+    }
 
     return Response.json(result);
 }
 
-function createPostOperation<TModel extends PrismaModelName>(
-    operation: Omit<PostOperation<TModel>, "operation">
-): PostOperation<TModel> {
+function createPostOperation<
+    TModel extends PrismaModelName,
+    TInputValidation extends Validation<TModel>
+>(
+    operation: Omit<PostOperation<TModel, TInputValidation>, "operation">
+): PostOperation<TModel, TInputValidation> {
     return {
         ...operation,
         operation: "post"
