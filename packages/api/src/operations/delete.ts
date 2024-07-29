0281639973a124c910fe8f743033e68d11a27a60
capiloky <jauregui.alvaro@nimioestudio.com>
2024-07-30 00:59:45 +0200

    refactor: update deleteHandler function to include post-persist operation

--- packages/api/src/operations/delete.ts
+++ packages/api/src/operations/delete.ts
@@ -2,12 +2,14 @@ import {
     Context,
     DeleteOperation,
     PrismaModelName,
-    UriVariables
+    UriVariables,
+    Validation
 } from "../types";
 
 async function deleteHandler<
     TModel extends PrismaModelName,
-    TContext extends Context<TModel>
+    TContext extends Context<TModel>,
+    TInputValidation extends Validation<TModel>
 >({
     request,
     model,
@@ -17,7 +19,7 @@ async function deleteHandler<
 }: {
     request: Request;
     model: PrismaModelName;
-    operation: DeleteOperation<TModel>;
+    operation: DeleteOperation<TModel, TInputValidation>;
     uriVariables: UriVariables;
     context: TContext;
 }): Promise<Response> {
@@ -31,12 +33,24 @@ async function deleteHandler<
         select
     });
 
+    if (operation.onPostPersist) {
+        await operation.onPostPersist({
+            data: result,
+            operation,
+            uriVariables,
+            context
+        });
+    }
+
     return Response.json(result);
 }
 
-function createDeleteOperation<TModel extends PrismaModelName>(
-    operation: Omit<DeleteOperation<TModel>, "operation">
-): DeleteOperation<TModel> {
+function createDeleteOperation<
+    TModel extends PrismaModelName,
+    TInputValidation extends Validation<TModel>
+>(
+    operation: Omit<DeleteOperation<TModel, TInputValidation>, "operation">
+): DeleteOperation<TModel, TInputValidation> {
     return {
         ...operation,
         operation: "delete"
